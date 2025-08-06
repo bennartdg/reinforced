84127585f38b838c1ebbdefc50e2082a3c96e49b
Bennart Gunawan <114302443+bennartdg@users.noreply.github.com>
2025-08-06 15:20:49 +0000

    app update 1.9 delete remove

--- app.py
+++ app.py
@@ -461,22 +461,24 @@ elif st.session_state.page == "hasil_evaluasi":
     else:
         st.warning("⚠️ Belum ada file penilaian ditemukan.")
 
+    # ! DELETE
     import os
     import glob
 
     folder_path = "penilaian"
     csv_files = glob.glob(os.path.join(folder_path, "*.csv"))
 
-    st.markdown("## 🗑️ Hapus File Penilaian")
+    # st.markdown("### 🗑️ Hapus File Penilaian")
 
-    for file in csv_files:
-        nama_file = os.path.basename(file)
-        col1, col2 = st.columns([0.8, 0.2])
-        with col1:
-            st.write(nama_file)
-        with col2:
-            if st.button(f"Hapus", key=nama_file):
-                os.remove(file)
-                st.success(f"✅ {nama_file} telah dihapus.")
-                st.rerun()
+    # for file in csv_files:
+    #     nama_file = os.path.basename(file)
+    #     col1, col2 = st.columns([0.8, 0.2])
+    #     with col1:
+    #         st.write(nama_file)
+    #     with col2:
+    #         if st.button(f"Hapus", key=nama_file):
+    #             os.remove(file)
+    #             st.success(f"✅ {nama_file} telah dihapus.")
+    #             st.rerun()
+    # ! DELETE
     # ! MAIN
